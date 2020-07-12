f4d36debba1564aa93f5fb7519d1efee2560dd39
dishvater <wwawrzyniak@gmail.com>
2020-07-12 11:16:07 +0200

    create user model

--- sportmate/smate/views.py
+++ sportmate/smate/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import CreateView
+
+from sportmate.smate.forms import UserForm
+from sportmate.smate.models import User
 
 
 def main_site_template(request):
     return render(request, "index.html", {})
+
+
+class PostCreateView(CreateView):
+    model = User
+    form_class = UserForm
+    success_url = "/user/add"
+    template_name = "add.html"
