da696fafb278fa10cfd87e3c934c2436e66e973f
dishvater <wwawrzyniak@gmail.com>
2020-07-12 09:33:12 +0200

    delete secret key

--- sportmate/sportmate/settings.py
+++ sportmate/sportmate/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'unsf9d2&b5s&ea3)h@b5=@0yh%zc(89!4#m=yhl=m^co7%ea$u'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
